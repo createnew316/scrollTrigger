da37e8e09b51c3d34467d927d043d3582fd0d9de
Anjali Namdev <115282386+createnew316@users.noreply.github.com>
2024-01-06 22:24:12 +0530

    Add files via upload
    add

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-gsap.to("#page2 h1",{
-    transform:"translateX(-125%)",
+/*gsap.from("#page1 #circle",{
+    scale:0,
+    delay:1,
+    duration:2,
+    rotate:360
+})
+gsap.from("#page2 #circle",{
+    scale:0,
+    delay:1,
+    duration:2,
+    rotate:360,
+   // scrollTrigger:"page2 #circle"
+   scrollTrigger:{
+    trigger:"#page2 #circle",
+    scroller:"body",
+    markers:true,
+    start:"top 60%",
+    end:"top 30%",
+    scrub:true
+   }
+}) */
+
+gsap.to("#page2 img",{
+    width:"100%",
     scrollTrigger:{
         trigger:"#page2",
-        scroll:"body",
-        scrub:2,
-        marker:true,
+        scroller:"body",
+        markers:true,
         start:"top 0",
         end:"top -100%",
-        pin:true
+        scrub:2,
+        pin:true,
     }
 })
